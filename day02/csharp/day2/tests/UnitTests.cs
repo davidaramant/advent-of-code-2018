d6f153194a4efed220b25238187db108da34c32b
David Aramant <davidaramant@gmail.com>
2018-12-02 11:43:44 -0500

    Empty project

---
+++ day02/csharp/day2/tests/UnitTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class UnitTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
