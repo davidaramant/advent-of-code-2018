53d57991477ba192ecdeb146af01c83f39466a4a
David Aramant <davidaramant@gmail.com>
2018-12-02 12:16:11 -0500

    Day 2 part 1 C#

--- day02/csharp/day2/day2/Program.cs
+++ day02/csharp/day2/day2/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace day2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ComputeChecksum(File.ReadLines("input.txt")));
+        }
+
+        public struct Result
+        {
+            public readonly bool ContainsDouble;
+            public readonly bool ContainsTriple;
+
+            public Result(bool containsDouble, bool containsTriple)
+            {
+                ContainsDouble = containsDouble;
+                ContainsTriple = containsTriple;
+            }
+
+            public Count ToIncrement() => new Count(ContainsDouble ? 1 : 0, ContainsTriple ? 1 : 0);
+        }
+
+        public struct Count
+        {
+            public readonly int NumDoubles;
+            public readonly int NumTriples;
+            
+            public Count(int numDoubles, int numTriples)
+            {
+                NumDoubles = numDoubles;
+                NumTriples = numTriples;
+            }
+
+            public static Count operator +(Count c1, Count c2)
+            {
+                return new Count(c1.NumDoubles + c2.NumDoubles, c1.NumTriples + c2.NumTriples);
+            }
+
+            public int GetChecksum() => NumDoubles * NumTriples;
+        }
+
+        public static int ComputeChecksum(IEnumerable<string> inputs)
+        {
+            return inputs
+                .Select(GetScore)
+                .Aggregate(
+                    new Count(),
+                    (counts, result) => counts + result.ToIncrement())
+                .GetChecksum();
+        }
+
+        public static Result GetScore(string input)
+        {
+            var dict = new Dictionary<char, int>();
+
+            foreach (var c in input)
+            {
+                if (!dict.ContainsKey(c))
+                {
+                    dict[c] = 0;
+                }
+                dict[c]++;
+            }
+
+            return new Result(dict.Values.Any(count => count == 2), dict.Values.Any(count => count == 3));
         }
     }
 }
--- day02/csharp/day2/tests/UnitTests.cs
+++ day02/csharp/day2/tests/UnitTests.cs
@@ -1,13 +1,51 @@
 using NUnit.Framework;
+using day2;
 
 namespace Tests
 {
     public class UnitTests
     {
         [Test]
-        public void Test1()
+        public void ShouldComputeChecksum()
         {
-            Assert.Pass();
+            var inputs = new[]
+            {
+                "abcdef",
+                "bababc",
+                "abbcde",
+                "abcccd",
+                "aabcdd",
+                "abcdee",
+                "ababab",
+            };
+
+            Assert.That(Program.ComputeChecksum(inputs), Is.EqualTo(12));
+        }
+
+        [TestCase("abcdef", false)]
+        [TestCase("bababc", true)]
+        [TestCase("abbcde", true)]
+        [TestCase("abcccd", false)]
+        [TestCase("aabcdd", true)]
+        [TestCase("abcdee", true)]
+        [TestCase("ababab", false)]
+        public void ShouldDetermineIfItContainsADouble(string input, bool expectedResult)
+        {
+            var containsDouble = Program.GetScore(input).ContainsDouble;
+            Assert.That(containsDouble, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("abcdef", false)]
+        [TestCase("bababc", true)]
+        [TestCase("abbcde", false)]
+        [TestCase("abcccd", true)]
+        [TestCase("aabcdd", false)]
+        [TestCase("abcdee", false)]
+        [TestCase("ababab", true)]
+        public void ShouldDetermineIfItContainsATriple(string input, bool expectedResult)
+        {
+            var containsTriple = Program.GetScore(input).ContainsTriple;
+            Assert.That(containsTriple, Is.EqualTo(expectedResult));
         }
     }
 }
