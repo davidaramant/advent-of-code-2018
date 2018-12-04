da5471ca2daf01c492f0d793be67a536bdd296d9
David Aramant <davidaramant@gmail.com>
2018-12-04 14:05:01 -0500

    Day 4 - Abomination of a solution

--- day04/Day04/Program.cs
+++ day04/Day04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
@@ -7,31 +8,55 @@ using System.Text.RegularExpressions;
 
 namespace Day04
 {
+    static class Extensions
+    {
+        public static TimeSpan Sum<T>(this IEnumerable<T> sequence, Func<T, TimeSpan> selector) =>
+            sequence.Aggregate(TimeSpan.Zero, (total, container) => total + selector(container));
+
+        public static T MaxElement<T, U>(this IEnumerable<T> sequence, Func<T, U> selector) where U : IComparable<U>
+        {
+            return sequence.Aggregate((i1, i2) => selector(i1).CompareTo(selector(i2)) == 1 ? i1 : i2);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Strategy 1: " + GetStrategy1Answer(File.ReadLines("input.txt").OrderBy(l => l)));
+            var shiftGroups =
+                ParseShifts(File.ReadLines("input.txt").OrderBy(l => l))
+                    .GroupBy(shift => shift.GuardId)
+                    .ToArray();
 
+            Console.WriteLine("Strategy 1: " + GetStrategy1Answer(shiftGroups));
+            Console.WriteLine("Part B: " + GetPart2Answer(shiftGroups));
+
+            Console.ReadKey();
         }
 
-        public static int GetStrategy1Answer(IEnumerable<string> sortedLines)
+        public static int GetStrategy1Answer(IEnumerable<IGrouping<int, Shift>> shiftGroups)
         {
-            var shifts = ParseShifts(sortedLines);
+            var groupOfSleepiestGuard =
+                shiftGroups.Select(group => (group, group.Sum(s => s.GetTotalTimeAsleep()))).MaxElement(tuple => tuple.Item2).group;
 
-            shifts.GroupBy(shift => shift.GuardId);
+            var minuteSleepCounts = new ConcurrentDictionary<int, int>();
 
-            // TODO:
-            // group by id
-            // get total sleep time across all shifts
-            // select the group with the largest duration
-            // find out the minute most often asleep
+            foreach (var minute in groupOfSleepiestGuard.SelectMany(shift => shift.GetMinutesAsleep()))
+            {
+                minuteSleepCounts.AddOrUpdate(minute, 1, (key, total) => total + 1);
+            }
 
-            // wrong
-            // Why is there no Max that returns the element????
-            var mostSleepy = shifts.OrderByDescending(shift => shift.GetTotalTimeAsleep()).First();
+            return groupOfSleepiestGuard.Key * minuteSleepCounts.MaxElement(pair => pair.Value).Key;
+        }
 
-            return 0;
+        public static int GetPart2Answer(IEnumerable<IGrouping<int, Shift>> shiftGroups)
+        {
+            var result = shiftGroups.Select(group =>
+               (id: group.Key,
+                mostSlept: group.Aggregate(MinutesSlept.Zero, (total, shift) => total + shift.GetMinutesSlept()).GetMostSleptMinute()))
+                .MaxElement(pair => pair.mostSlept.Times);
+
+            return result.id * result.mostSlept.Minute;
         }
 
         private static IEnumerable<Shift> ParseShifts(IEnumerable<string> sortedLines)
@@ -81,6 +106,18 @@ namespace Day04
         }
     }
 
+    public struct SleepMinute
+    {
+        public int Minute { get; }
+        public int Times { get; }
+
+        public SleepMinute(int minute, int times)
+        {
+            Minute = minute;
+            Times = times;
+        }
+    }
+
     public struct SleepTime
     {
         public DateTime Start { get; }
@@ -98,13 +135,67 @@ namespace Day04
         public int GuardId { get; }
         public ImmutableList<SleepTime> TimesAsleep { get; }
 
-        public TimeSpan GetTotalTimeAsleep() =>
-            TimesAsleep.Aggregate(TimeSpan.Zero, (total, sleepTime) => total + sleepTime.Duration);
+        public TimeSpan GetTotalTimeAsleep() => TimesAsleep.Sum(s => s.Duration);
+
+        public IEnumerable<int> GetMinutesAsleep()
+        {
+            foreach (var sleepTime in TimesAsleep)
+            {
+                var start = sleepTime.Start.Minute;
+                var duration = (int)sleepTime.Duration.TotalMinutes;
+                for (int minute = start; minute < start + duration; minute++)
+                {
+                    yield return minute;
+                }
+            }
+        }
 
         public Shift(int guardId, IEnumerable<SleepTime> sleepTimes)
         {
             GuardId = guardId;
             TimesAsleep = sleepTimes.ToImmutableList();
+        }
+
+        public MinutesSlept GetMinutesSlept() =>
+            TimesAsleep.Aggregate(MinutesSlept.Zero, (total, sleepTime) => total + MinutesSlept.From(sleepTime));
+    }
+
+    public sealed class MinutesSlept
+    {
+        private readonly int[] _minutes;
+
+        public static readonly MinutesSlept Zero = new MinutesSlept(new int[60]);
+
+        private MinutesSlept(int[] minutes)
+        {
+            _minutes = minutes;
+        }
+
+        public static MinutesSlept From(SleepTime sleepTime)
+        {
+            var minutes = new int[60];
+            foreach (var index in Enumerable.Range(sleepTime.Start.Minute, (int)sleepTime.Duration.TotalMinutes))
+            {
+                minutes[index] = 1;
+            }
+
+            return new MinutesSlept(minutes);
+        }
+
+        public static MinutesSlept operator +(MinutesSlept ms1, MinutesSlept ms2)
+        {
+            var sum = new int[60];
+            for (int i = 0; i < 60; i++)
+            {
+                sum[i] = ms1._minutes[i] + ms2._minutes[i];
+            }
+
+            return new MinutesSlept(sum);
+        }
+
+        public SleepMinute GetMostSleptMinute()
+        {
+            return _minutes.Select((count, index) => new SleepMinute(index, count)).MaxElement(min => min.Times);
         }
     }
 }
